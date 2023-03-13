10833a2eb644e6c188be07a1a3f17b1596d16f4f
hamzahafaiedh <hamza.hafaiedh@esprit.tn>
2023-03-13 22:54:20 +0100

    modifications

---
+++ hospihub-frontend/src/Components/FormsComponent/ErrorSignInPage.js
@@ -0,0 +1,11 @@
+import './ErrorSignInPage.css';
+
+function ErrorSignInPage() {
+    return (  
+        <>
+        <img src='../assetsTemplates/templateForm/images/errorsignin.png' className="img-fluid offset-md-1" style={{ width: "70%", height: "70%"}} alt=""/>
+        </>
+    );
+}
+
+export default ErrorSignInPage;
