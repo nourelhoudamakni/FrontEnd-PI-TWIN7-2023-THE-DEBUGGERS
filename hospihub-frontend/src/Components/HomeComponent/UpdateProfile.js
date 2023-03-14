9ddc93ed8438b939894b2612b617923671ed7d30
houssemkharroubi <houssem.kharroubiessayed@esprit.tn>
2023-03-14 04:19:39 +0100

    + form control update profile

--- hospihub-frontend/src/Components/HomeComponent/UpdateProfile.js
+++ hospihub-frontend/src/Components/HomeComponent/UpdateProfile.js
@@ -3,13 +3,17 @@ import jwt_decode from "jwt-decode";
 import axios from "axios";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Alert from "react-bootstrap/Alert";
 
 function UpdateProfile() {
 
     const [User, setUser] = useState({});
     const [MedicalRecord, setMedicalRecord] = useState({});
- 
+    const [UsernameErrorMessage, setUsernameErrorMessage] = useState('');
+    const [LastNameErrorMessage, setLastNameErrorMessage] = useState('');
+    const [PhoneNumberErrorMessage, setPhoneNumberErrorMessage] = useState('');
 
+    //onBlur={() =>setPhoneNumberErrorMessage(!/^\d{8}$/.test(User.phoneNumber)) } required
     useEffect(() => {
         const token = localStorage.getItem('jwtToken');
         if (token) {
@@ -47,6 +51,20 @@ function UpdateProfile() {
 
     const handleUpdateProfile = (e) => {
         e.preventDefault();
+        if (!/^[a-zA-Z\s]+$/.test(User.firstName) || !/^[a-zA-Z\s]+$/.test(User.lastName)) {
+            setLastNameErrorMessage(true);
+            return;
+          }
+        if (!/^[a-zA-Z0-9]{3,}$/.test(User.userName)) {
+            setUsernameErrorMessage(true);
+            return;
+          }
+          if (!/^\d{8}$/.test(User.phoneNumber)) {
+            setPhoneNumberErrorMessage(true);
+            return;
+          }
+
+          
         const token = localStorage.getItem('jwtToken');
         if (token) {
             const decodedToken = jwt_decode(token);
@@ -80,23 +98,49 @@ function UpdateProfile() {
                             {/* Form Group (username)*/}
                             <div className="mb-3">
                                 <label className="small mb-1" htmlFor="inputUsername" >Username (how your name will appear to other users on the Website)</label>
-                                <input className="form-control" id="inputUsername" type="text" placeholder="Enter your username" name='userName' value={User.userName} onChange={(e) => onValueChange(e)} />
+                                <input className="form-control" id="inputUsername" type="text" placeholder="Enter your username" name='userName' value={User.userName} onChange={(e) => onValueChange(e)} onBlur={() =>setUsernameErrorMessage(!/^[a-zA-Z0-9]{3,}$/.test(User.userName)) } required />
                             </div>
-
+                            {UsernameErrorMessage && (
+                    <Alert
+                      className="form-group"
+                      variant="danger"
+                      style={{ marginTop: "-13px" }}
+                    >
+                      <div
+                        className="form-icon-wrapper  text-danger"
+                        style={{ marginTop: "-11px", marginBottom: "-13px" }}
+                      >
+                       Username must contain at least 3 characters and should not contain special characters
+                      </div>
+                    </Alert>
+                  )}
                             {/* Form Row*/}
                             <div className="row gx-3 mb-3">
                                 {/* Form Group (first name)*/}
                                 <div className="col-md-6">
                                     <label className="small mb-1" htmlFor="inputFirstName">First name</label>
-                                    <input className="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" name='firstName' value={User.firstName} onChange={(e) => onValueChange(e)} />
+                                    <input className="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" name='firstName' value={User.firstName} onChange={(e) => onValueChange(e)} onBlur={() =>setLastNameErrorMessage(!/^[a-zA-Z\s]+$/.test(User.firstName) || !/^[a-zA-Z\s]+$/.test(User.lastName))} required />
                                 </div>
                                 {/* Form Group (last name)*/}
                                 <div className="col-md-6">
                                     <label className="small mb-1" htmlFor="inputLastName">Last name</label>
-                                    <input className="form-control" id="inputLastName" type="text" placeholder="Enter your last name" name='lastName' value={User.lastName} onChange={(e) => onValueChange(e)} />
+                                    <input className="form-control" id="inputLastName" type="text" placeholder="Enter your last name" name='lastName' value={User.lastName} onChange={(e) => onValueChange(e)} onBlur={() =>setLastNameErrorMessage(!/^[a-zA-Z\s]+$/.test(User.firstName) || !/^[a-zA-Z\s]+$/.test(User.lastName))} required />
                                 </div>
                             </div>
-
+                            {LastNameErrorMessage && (
+                    <Alert
+                      className="form-group"
+                      variant="danger"
+                      style={{ marginTop: "-13px" }}
+                    >
+                      <div
+                        className="form-icon-wrapper  text-danger"
+                        style={{ marginTop: "-11px", marginBottom: "-13px" }}
+                      >
+                       First name and Last name should only contains alphabetical characters.
+                      </div>
+                    </Alert>
+                  )}
                             <div className="row gx-3 mb-3">
                                 <div className="col-md-4">
                                     <label className="small mb-1" htmlFor="inputDateofbirth">Date of birth</label>
@@ -117,9 +161,23 @@ function UpdateProfile() {
                                 {/* Form Group (phone number)*/}
                                 <div className="col-md-6">
                                     <label className="small mb-1" htmlFor="inputPhone" >Phone number</label>
-                                    <input className="form-control" id="inputPhone" type="tel" placeholder="Enter your phone number" name='phoneNumber' value={User.phoneNumber} onChange={(e) => onValueChange(e)} required />
+                                    <input className="form-control" id="inputPhone" type="tel" placeholder="Enter your phone number" name='phoneNumber' value={User.phoneNumber} onChange={(e) => onValueChange(e)} onBlur={() =>setPhoneNumberErrorMessage(!/^\d{8}$/.test(User.phoneNumber)) } required />
                                 </div>
                             </div>
+                            {PhoneNumberErrorMessage && (
+                    <Alert
+                      className="form-group"
+                      variant="danger"
+                      style={{ marginTop: "-13px" }}
+                    >
+                      <div
+                        className="form-icon-wrapper  text-danger"
+                        style={{ marginTop: "-11px", marginBottom: "-13px" }}
+                      >
+                       First name and Last name should only contains alphabetical characters.
+                      </div>
+                    </Alert>
+                  )}
 
                             {/* Save changes button*/}
                             <button className="btn btn-primary" type="button" onClick={handleUpdateProfile}>Save changes</button>
