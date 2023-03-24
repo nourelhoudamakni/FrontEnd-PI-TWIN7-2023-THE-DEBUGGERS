dd291f0731026921e79f130ec2c9235a4e2698d7
hamzahafaiedh <hamza.hafaiedh@esprit.tn>
2023-03-25 00:36:29 +0100

    show password

--- hospihub-frontend/src/Components/FormsComponent/SignInComponent.js
+++ hospihub-frontend/src/Components/FormsComponent/SignInComponent.js
@@ -7,6 +7,9 @@ import Alert from "react-bootstrap/Alert";
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
 function SignInComponent() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -18,6 +21,12 @@ function SignInComponent() {
   const [errorPasswordMessage, setPasswordErrorMessage] = useState(false);
   const [errorSecretMessage, setSecretErrorMessage] = useState(false);
   const navigate = useNavigate();
+
+
+  const [password1, setPassword1] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => setShowPassword(!showPassword);
 
   //passport Sign In 
   // const navigate = useNavigate();
@@ -229,21 +238,26 @@ function SignInComponent() {
                     </Alert>
                   )}
 
-                  <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <div className="form-icon-wrapper">
-                      <input
-                        type="password"
-                        className="form-control"
-                        id="password"
-                        placeholder="Enter password"
-                        required
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                      />
-                      <i className="form-icon-left mdi mdi-lock" />
-                    </div>
-                  </div>
+
+{/* //////////////////////////////// */}
+<div className="form-group">
+      <label htmlFor="password">Password</label>
+      <div className="form-icon-wrapper">
+        <input
+          type={showPassword ? 'text' : 'password'}
+          className="form-control"
+          id="password"
+          placeholder="Enter password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <i
+          className={`form-icon-left mdi mdi-${showPassword ? 'eye' : 'eye-off'}`}
+          onClick={toggleShowPassword}
+        />
+      </div>
+    </div>
 
                   {errorPasswordMessage && (
                     <Alert
